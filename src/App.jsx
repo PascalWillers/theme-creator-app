24a1e6a61d2f425874fffa59d7f4fc169416c3ec
PascalWillers <164835421+PascalWillers@users.noreply.github.com>
2024-05-02 14:40:46 +0200

    Revert "2- add color"

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,17 @@
 import { initialColors } from "./lib/colors";
 import Color from "./Components/Color/Color";
 import "./App.css";
-import { useState } from "react";
-import { uid } from "uid";
-import ColorForm from "./Components/ColorForm/ColorForm";
+
 function App() {
-  const [colors, setColors] = useState(initialColors);
-  function handleAddColor(color) {
-    setColors([{ ...color, id: uid() }, ...colors]);
-  }
   return (
     <>
       <h1>Theme Creator</h1>
-      <ColorForm onSubmitColor={handleAddColor} />
-      {colors.map((color) => (
-        <Color key={color.id} color={color} />
-      ))}
+
+      {initialColors.map((color) => {
+        return <Color key={color.id} color={color} />;
+      })}
     </>
   );
 }
+
 export default App;
