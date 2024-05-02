52a9060f62cdab1ed079cac0289659296a2e42c1
Pascal Willers <pascal-willers@gmx.de>
2024-05-02 12:00:31 +0200

    2- add color

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import { initialColors } from "./lib/colors";
 import Color from "./Components/Color/Color";
 import "./App.css";
-
+import { useState } from "react";
+import { uid } from "uid";
+import ColorForm from "./Components/ColorForm/ColorForm";
 function App() {
+  const [colors, setColors] = useState(initialColors);
+  function handleAddColor(color) {
+    setColors([{ ...color, id: uid() }, ...colors]);
+  }
   return (
     <>
       <h1>Theme Creator</h1>
-
-      {initialColors.map((color) => {
-        return <Color key={color.id} color={color} />;
-      })}
+      <ColorForm onSubmitColor={handleAddColor} />
+      {colors.map((color) => (
+        <Color key={color.id} color={color} />
+      ))}
     </>
   );
 }
-
 export default App;
