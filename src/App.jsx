c3617b490df90b583deef9fe19a41105bd1097a1
Pascal Willers <pascal-willers@gmx.de>
2024-05-03 11:34:12 +0200

    deleteColor-button

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
 import { initialColors } from "./lib/colors";
 import Color from "./Components/Color/Color";
 import "./App.css";
-
+import { useState } from "react";
+import { uid } from "uid";
+import ColorForm from "./Components/ColorForm/ColorForm";
 function App() {
+  const [colors, setColors] = useState(initialColors);
+  function handleAddColor(color) {
+    setColors([{ ...color, id: uid() }, ...colors]);
+  }
+  function handleDeleteColor(id) {
+    const updatedColors = colors.filter((color) => color.id !== id);
+    setColors(updatedColors);
+  }
   return (
     <>
       <h1>Theme Creator</h1>
-
-      {initialColors.map((color) => {
-        return <Color key={color.id} color={color} />;
-      })}
+      <ColorForm onSubmitColor={handleAddColor} />
+      {colors.map((color) => (
+        <Color key={color.id} color={color} onDeleteColor={handleDeleteColor} />
+      ))}
     </>
   );
 }
-
 export default App;
--- src/Components/Color/Color.jsx
+++ src/Components/Color/Color.jsx
@@ -1,17 +1,36 @@
 import "./Color.css";
-
-export default function Color({ color }) {
+import { useState } from "react";
+export default function Color({ onDeleteColor, color }) {
+  const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false);
+  function onDelete() {
+    setShowDeleteConfirmation(true);
+  }
+  function onCancelDelete() {
+    setShowDeleteConfirmation(false);
+  }
+  function onConfirmDelete(id) {
+    onDeleteColor(id);
+    setShowDeleteConfirmation(false);
+  }
   return (
     <div
       className="color-card"
-      style={{
-        background: color.hex,
-        color: color.contrastText,
-      }}
+      style={{ background: color.hex, color: color.contrastText }}
     >
       <h3 className="color-card-headline">{color.hex}</h3>
       <h4>{color.role}</h4>
       <p>contrast: {color.contrastText}</p>
+      <div>
+        {showDeleteConfirmation ? (
+          <div>
+            <h3 className="color-card-highlight">Really delete?</h3>
+            <button onClick={onCancelDelete}>Cancel</button>
+            <button onClick={() => onConfirmDelete(color.id)}>Confirm</button>
+          </div>
+        ) : (
+          <button onClick={onDelete}>Delete</button>
+        )}
+      </div>
     </div>
   );
 }
